cde467f4911fbccc6f2e36fa4db8d7858055221e
Nathan Krasney <natankrasney@gmail.com>
2022-06-29 00:02:42 +0300

    signin signout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-import {signIn,signOut} from 'next-auth/react';
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const { data: session, status } = useSession();
 
-  
+  console.log(status === "authenticated" ? "signin" : "signout");
+
   return (
     <div className={styles.container}>
-      <button onClick={() => signIn()}>SignIn</button>
+      <button onClick={() => signIn("github")}>SignIn</button>
       <button onClick={() => signOut()}>SignOut</button>
+      <p>{status === "authenticated" ? 'signin' : 'signout'}</p>
     </div>
   );
 };
